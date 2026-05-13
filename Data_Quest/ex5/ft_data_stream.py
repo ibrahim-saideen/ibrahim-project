830205fbb9081dfaadac3d6356310e3d741bcee4
ibrahim saideen <saideenibrahim@gmail.com>
2026-05-13 14:47:04 -0700

    its all done but without handle flake8 and mypy

--- Data_Quest/ex5/ft_data_stream.py
+++ Data_Quest/ex5/ft_data_stream.py
@@ -2,6 +2,14 @@ import random
 import typing
 
 
+def consume_event(lst_of_tbls):
+    tbl = ()
+    len_lst = len(lst_of_tbls)
+    for _ in range(len_lst):
+        tbl = random.choice(lst_of_tbls)
+        yield tbl
+
+
 def gen_event():
     rand_names  = ['alice', 'bob', 'charlie', 'dylan']
     rand_actions = ['run', 'sleep', 'move', 'grab', 'climp', 'swim', 'eat', 'release']
@@ -19,8 +27,19 @@ def main() ->None :
         tbl = next(events)
         print(f'Event {i}:  ',end='')
         print(f'Player {tbl[0]} did action {tbl[1]}')
-    
-
+    tt = gen_event()
+    lst_of_tbls = []
+    for _ in range(10):
+        tbl = next(tt)
+        lst_of_tbls.append(tbl)
+    print(f'Built list of 10 events:  {lst_of_tbls}')
+    consume = consume_event(lst_of_tbls)
+    len_lst = len(lst_of_tbls)
+    for _ in range(len_lst):
+        tbll = next(consume)
+        print(f'Got event from list:  {tbll}')
+        lst_of_tbls.remove(tbll)
+        print(f'Remains in list:  {lst_of_tbls}')
 
 
 if __name__ == '__main__':
